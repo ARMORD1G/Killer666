da1988c30279c7e774dea38b419be0712cc11001
Twilight Sparkle <78149818+ARMORD1G@users.noreply.github.com>
2024-09-27 22:03:55 +0300

    Add files via upload

---
+++ ZOV/src/test/java/ru/mtuci/ZOV/ZovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.ZOV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
